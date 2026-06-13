480a95797ac252350da6250239293a515d62b6e7
lijinyang1028 <lijinyang091028@163.com>
2026-06-13 15:44:02 +0800

    紧急修复

--- websearcher.py
+++ websearcher.py
@@ -120,6 +120,6 @@ def main():
 if __name__ == "__main__":
     main()
     
-if __name__ == "__main__":
+'''if __name__ == "__main__":
     trying_window = WebSearcherGUI()
-    trying_window.mainpage()
+    trying_window.mainpage()'''
